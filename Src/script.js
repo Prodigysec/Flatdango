b55c5b7d4ca4f83ad68372b22d03f5966113a3f3
Ian Raburu <williamraburu@gmail.com>
2023-07-02 21:55:57 +0300

    Define and populate arrays

--- Src/script.js
+++ Src/script.js
@@ -5,4 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
     scrollButton.addEventListener('click', function () {
         movieContainer.scrollIntoView({ behavior: 'smooth' });
     });
+
+    const filmsList = document.getElementsByClassName("films-list");
+
+    fetch("http://localhost:3000/films")
+        .then(result => { return result.json(); })
+        .then(data => {
+            let individualFilmTitle = [];
+            let individualFilmRuntime = [];
+            let individualFilmCapacity = [];
+            let individualFilmShowtime = [];
+            let individualFilmTicketsSold = [];
+            let individualFilmDescription = [];
+            let individualFilmPoster = [];
+
+            for (const key in data) {
+                individualFilmTitle.push(data[key].title);
+                individualFilmRuntime.push(data[key].runtime);
+                individualFilmCapacity.push(data[key].capacity);
+                individualFilmShowtime.push(data[key].showtime);
+                individualFilmTicketsSold.push(data[key].tickets_sold);
+                individualFilmDescription.push(data[key].description);
+                individualFilmPoster.push(data[key].poster);
+            }
+
+        });
 });
